32cfe20141623b5fdc2e3e077415a20017793d2f
Fengalf <14805375+Fengalf@users.noreply.github.com>
2023-10-10 17:04:12 +0200

    adding readme, relocating gitignore, script store

---
+++ README.md
@@ -0,0 +1,6 @@
+# What are you seeing here?
+
+---
+
+This is my very own version of a coffee maker which was a task in a python learning course (100-days of python).
+Both pdfs that are included here are created by the owner of that course.
---
+++ main_oop.py
@@ -0,0 +1,16 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+
+def coffe_machine():
+    main_menu = Menu()
+    # menu_items = MenuItem()
+    # coffe_brewer = CoffeeMaker()
+    # money_handler = MoneyMachine()
+
+    main_menu.menu
+
+
+if __name__ == "__main__":
+    coffe_machine()
